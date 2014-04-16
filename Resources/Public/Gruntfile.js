39978363a37b20dac6b05233a1cc2d4d6ed292d1
ryzy <marcin@ryzycki.com>
2014-04-17 00:23:40 +0100

    foundation installed

---
+++ Resources/Public/Gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function (grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
+		sass: {
+			options: {
+				includePaths: ['vendors/foundation/scss'],
+				sourceComments: 'map'
+			},
+			dist: {
+				options: {
+					outputStyle: 'compressed'
+				},
+				files: {
+					'css/app.css': 'scss/app.scss'
+				}
+			}
+		},
+
+		watch: {
+			grunt: { files: ['Gruntfile.js'] },
+
+			sass: {
+				files: 'scss/**/*.scss',
+				tasks: ['sass']
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	grunt.registerTask('build', ['sass']);
+	grunt.registerTask('default', ['build', 'watch']);
+}
