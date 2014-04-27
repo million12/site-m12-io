e57046385e718e81922962ff8047eddbfca2bd8c
ashB <ashb100@outlook.com>
2014-04-27 16:23:03 +0100

    autoprefixer added to gulp

--- Build/gulpfile.js
+++ Build/gulpfile.js
@@ -21,6 +21,7 @@ gulp.task('sass', function() {
 			sourceComments: 'map',
 			outputStyle: 'nested'
 		}))
+		.pipe(plugins.autoprefixer('last 3 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
 		.pipe(gulp.dest(destDir + '/Styles'))
 		.pipe(plugins.connect.reload())
 	;
