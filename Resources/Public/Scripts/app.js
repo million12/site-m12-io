bd862de6f9f8b8766fda7a5783184345545b95d7
Ashnita Bali <ashnita01@gmail.com>
2014-07-19 16:38:30 +0100

    Changed from using Singleton Pattern to Revealing Object Module Pattern for organising JavaScript code.

--- Resources/Public/Scripts/app.js
+++ Resources/Public/Scripts/app.js
@@ -7,27 +7,42 @@ $(document).ready(function() {
 	M12.Site.Scrolling.init();
 });
 
-var M12 = {};
-M12.Site = {
-	Animation: {
+/**
+ * M12 Namespace
+ */
+var M12 = function() {
+	return {};
+}();
+
+/**
+ * M12.Site Namespace
+ * @returns {Object}.<string, function>:Animation, Scrolling
+ */
+M12.Site = function() {
+	/**
+	 * Animation Module
+	 * @return {Object.<string, function>}
+ 	 */
+	var Animation = function() {
+
+		var init = function() {
+			animateSplashScreen();
+		};
+
 		/**
 		 * Animate the list items so that one item is visible and moves out of view and the next list item is shown.
 		 * The animation goes through all the list items in the unordered list and starts from the first list item again.
 		 * To show only one list item at a time, the height is set on the ul and overview is set to hidden.
 		 * The next list item is shown by changing the margin-top to (height of ul * -(the item's position in the list)
+		 *
+		 * @return {void}
 		 */
-		init: function() {
-			this.animateSplashScreen();
-		},
-		animateSplashScreen: function() {
+		var animateSplashScreen = function() {
 			// keep references to html elements so that I can give it to jQuery selectors
-
-			//var parentDiv = $('.animatedList'),
-
 			var ulEl = $('ul', $('.animatedList')),
-			    height = ulEl.height(),
-			    numberItems = ulEl.children().length,
-			    first = ulEl.children().first(),
+				height = ulEl.height(),
+				numberItems = ulEl.children().length,
+				first = ulEl.children().first(),
 				currentItem = 1,
 				marginTop = 0;
 
@@ -39,25 +54,47 @@ M12.Site = {
 					currentItem = 1;
 				} else currentItem++;
 			}, 3000);
+		};
+
+		// Public function
+		return {
+			init: init
 		}
-	},
+	}();
 
 	/**
-	 * Use jQuery.localScroll plugin for smooth scrolling. Specify the element contain our local links
-	 * (if not specified, includes all the local links). Hash settings are passed to jQuery.scrollTo fo the animation.
+	 * Scrolling Module
+	 * @return {object.<string, function>}
 	 */
-	Scrolling: {
-		init: function() {
-			this.initSmoothScrolling();
-		},
+	var Scrolling = function() {
+		var init = function() {
+			initSmoothScrolling();
+		};
 
-		initSmoothScrolling: function(){
+		/**
+		 * Use jQuery.localScroll plugin for smooth scrolling. Specify the element contain our local links.
+		 * If element not specified, includes all the local links. Hash settings are passed to jQuery.scrollTo for
+		 * the animation.
+		 * @return {void}
+		 */
+		var initSmoothScrolling = function(){
 			$('.top-bar, .splashScreen').localScroll({
 				target:'body',
 				duration:'150',
 				ease:'swing'
 			});
+		};
+
+		// Public function
+		return {
+			init: init
 		}
-	}
-};
+	}();
+
+	// Return the Modules within M12.Site
+	return {
+		Animation: Animation,
+		Scrolling: Scrolling
+	};
+}();
 
