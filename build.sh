46385c3d73841a482e103c16db78f75ce4f7e018
ryzy <marcin@ryzycki.com>
2014-10-25 16:58:56 +0100

    build.sh re-written so it support --preinstall flag
    This reflects most recent changes in million12/typo3-neos-abstract base containers, where this script is called with '--preinstall' param. This allows us to install time consuming things during image build process, instead of during container run time.

--- build.sh
+++ build.sh
@@ -1,21 +1,40 @@
 #!/bin/sh
 #
-# M12.io build script
+# Site build script
 #
 # This file should contain all necessary step to build up&running website.
 # Include here all necessary build steps (e.g. scripts minification,
 # styles compilation etc).
 #
 
-echo "M12.IO build script"
-
-# This should be already available in the system - install it manually if needed.
-#sudo npm install -g gulp bower
-
-cd Build/
-
-bower install
-npm install
-gulp build --env=Production # build for production by default
+case $@ in
+  #
+  # This is called when container is being build (and this script is called with --preinstall param)
+  #
+  *--preinstall*)
+    echo "M12.IO build script: PREINSTALL"
+    
+    # Install required tools globally
+    npm install -g gulp bower
+    
+    # Install site packages
+    set -e # exit with error if any of the following fail
+    cd Build/
+    bower install --allow-root
+    npm install
+    gulp build --env=Production
+    ;;
+ 
+  #
+  # This is called when container launches (and script is called without param)
+  #
+  *)
+    echo "M12.IO build script"
+    cd Build/
+    bower install
+    npm install
+    gulp build --env=Production # build for production by default
+    ;;
+esac
 
 echo "M12.IO Build completed."
